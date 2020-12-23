5faafbfcb5f407941be8316a110915498403d851
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-12-23 20:49:21 +0100

    !

--- drasyl-core/src/main/java/org/drasyl/remote/handler/InvalidProofOfWorkFilter.java
+++ drasyl-core/src/main/java/org/drasyl/remote/handler/InvalidProofOfWorkFilter.java
@@ -28,6 +28,7 @@ import org.drasyl.util.ReferenceCountUtil;
 import org.drasyl.util.logging.Logger;
 import org.drasyl.util.logging.LoggerFactory;
 
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
 import static org.drasyl.identity.IdentityManager.POW_DIFFICULTY;
@@ -51,18 +52,19 @@ public class InvalidProofOfWorkFilter extends SimpleInboundHandler<IntermediateE
                                final IntermediateEnvelope<MessageLite> msg,
                                final CompletableFuture<Void> future) {
         try {
-            if (msg.getProofOfWork().isValid(msg.getSender(), POW_DIFFICULTY)) {
+            if (msg.isChunk() || msg.getProofOfWork().isValid(msg.getSender(), POW_DIFFICULTY)) {
                 ctx.fireRead(sender, msg, future);
             }
             else {
                 LOG.trace("Message with invalid proof of work dropped: {}", msg);
                 future.completeExceptionally(new Exception("Message with invalid proof of work dropped"));
+                ReferenceCountUtil.safeRelease(msg);
             }
         }
-        catch (final IllegalArgumentException e) {
-            LOG.error("Unable to read sender from message '{}': {}", sanitizeLogArg(msg), e.getMessage());
+        catch (final IllegalArgumentException | IOException e) {
+            LOG.debug("Message {} can't be read and was dropped due to the following error: ", msg, e);
+            future.completeExceptionally(new Exception("Message can't be read and was dropped due to the following error: ", e));
             ReferenceCountUtil.safeRelease(msg);
-            future.completeExceptionally(new Exception("Unable to read sender from message.", e));
         }
     }
 }
